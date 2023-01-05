ca5068af7b4d152a779e48ad066f14115246e19f
Tobani <tobanigeorge@gmail.com>
2023-01-05 02:59:54 +0100

    set up basic index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,13 @@
+import { config } from "dotenv";
+import cors from "cors";
+import express from "express";
 
+config();
+const app = express();
+const PORT = process.env.PORT ?? 9000;
+
+app.use(cors(), express.json());
+
+app.listen(PORT, () => {
+  console.log(`server is live (Port: ${PORT})`);
+});
