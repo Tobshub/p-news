e5251b379c63396190066656d9d27262bc59404b
Tobani <tobanigeorge@gmail.com>
2023-01-05 05:00:33 +0100

    init sidebar nav

--- client/src/components/ui/sidebar.tsx
+++ client/src/components/ui/sidebar.tsx
@@ -1,9 +1,33 @@
+import { NavLink } from "react-router-dom";
+
 export default function Sidebar(props: { user: {} }) {
   return (
-    <article className="">
-      {/* logo */}
-      {/* nav links */}
-      {/* user details */}
-    </article>
+    <nav className="navbar navbar-expand-lg">
+      <h1 className="navbar-brand">Logo</h1>
+      <div className="navbar-nav">
+        <BootNavLink to="/">Home</BootNavLink>
+        <BootNavLink to="/explore">Explore</BootNavLink>
+        <BootNavLink to="/notifications">Notifications</BootNavLink>
+        <BootNavLink to="/saved">Saved</BootNavLink>
+        <BootNavLink to="my-profile">My Profile</BootNavLink>
+      </div>
+    </nav>
+  );
+}
+
+interface BootNavLinkProps extends React.PropsWithChildren {
+  to: string;
+}
+function BootNavLink(props: BootNavLinkProps) {
+  return (
+    <NavLink
+      to={props.to}
+      className={({ isActive }) =>
+        `nav-item nav-link ${isActive ? "active" : ""}`
+      }
+      style={{ display: "block" }}
+    >
+      {props.children}
+    </NavLink>
   );
 }
