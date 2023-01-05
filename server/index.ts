b6d8c40e05bd08a9819c80962dd91754563326ae
Tobani <tobanigeorge@gmail.com>
2023-01-05 02:41:29 +0100

    removed demo code

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,1 @@
-import fs from "fs";
-import Reader from "word-extractor";
 
-const fileLocation = "C:/Users/HP/Documents/actual reflection.docx";
-
-fs.readFile(fileLocation, (err, data) => {
-  if (err) throw err;
-
-  const docReader = new Reader();
-  docReader
-    .extract(data)
-    .then(val => console.log(val.getBody()))
-    .catch(e => console.error(e));
-});
